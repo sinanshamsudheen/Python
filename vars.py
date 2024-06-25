df91759db228bcf2067939cde647c48663fc1445
Muhammed Sinan D <sinanshamsudheen7@gmail.com>
2024-06-25 20:58:29 +0530

    variables

--- vars.py
+++ vars.py
@@ -4,6 +4,13 @@ name = "Sinan"
 age = 20
 value = 0
 
-print("My name is : ",name)
+old = False
+
+size = None
+
+print("My name is : ",type(name))
 print("My age is : ",age)
 print("My current value is :",value)
+
+print(type(old))
+print(type(size))
