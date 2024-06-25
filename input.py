e2a8354bfd787a80fe9c9d3c490f4e1d25290d64
Muhammed Sinan D <sinanshamsudheen7@gmail.com>
2024-06-25 21:35:38 +0530

    input syntax

---
+++ input.py
@@ -0,0 +1,7 @@
+name = input("enter the name : ")
+
+age = int(input("enter the age : "))
+
+value = float(input("enter the value :"))
+
+print("My name is ",name,"Iam",age,"years old.","And my value is",value)
