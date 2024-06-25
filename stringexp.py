43ce59eb2716afb8eede1a7e5a45ea77082649fc
Muhammed Sinan D <sinanshamsudheen7@gmail.com>
2024-06-25 20:57:54 +0530

    fn on strings

---
+++ stringexp.py
@@ -0,0 +1,25 @@
+a=3
+b=2
+txt="#"
+
+print(a*txt*b)
+
+c,d="2",4
+
+print((c+txt)*d)
+
+# Integer division (//)
+x,y=3,2
+z=x//y
+
+print(z,x/y)
+# O/P= 1,1.5
+# floor(x/y) == Integer division
+
+A,B=4,-5
+print(A%B)
+print(B%A)
+
+"""python
+comments 
+example"""
