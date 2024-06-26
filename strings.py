1904277f7e05fb91a4b0ce3307c1b15af134fe03
Muhammed Sinan D <sinanshamsudheen7@gmail.com>
2024-06-26 22:28:59 +0530

    slicing

---
+++ strings.py
@@ -0,0 +1,16 @@
+#ways to declare & initialize a string
+str1="hello world"
+str2='hello bro'
+str3="""hello mom"""
+
+len1 = len(str1)
+print(len1)
+
+ch = str2[2]
+print(ch)
+
+# str2[3]="b" #Manipulation is not possible
+
+print(str3[0:5]) #slicing
+print(str3[:5])
+#ending index is not included
