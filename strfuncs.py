ff76a9cd9f45e791b5335b188549aff5d49e50d6
Muhammed Sinan D <sinanshamsudheen7@gmail.com>
2024-06-26 22:44:02 +0530

    string functions

---
+++ strfuncs.py
@@ -0,0 +1,11 @@
+str="mitochondria is the powerhouse of a cell"
+
+print(str.endswith("ell"))
+
+print(str.capitalize())
+
+print(str.find("o"))
+
+print(str.replace("the","a"))
+
+print(str.count("i"))
