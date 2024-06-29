f8ce84889e94ea21875c88c88046dbc4bd447673
Muhammed Sinan D <sinanshamsudheen7@gmail.com>
2024-06-29 10:07:04 +0530

    functions

---
+++ functions.py
@@ -0,0 +1,16 @@
+# built-in-functions
+
+print("sinan",end="@")
+print("liza") #end= "\n"
+print("sahal")
+
+books = ["english","korean","japanese","english","korean","japanese"]
+for i in books:
+    print(i,end=" ")
+print(len(books))
+
+def sum(a=4,b=5): #default parameters
+    print(a+b)
+sum(10,10)
+sum()
+
