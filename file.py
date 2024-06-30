a3f14601e938dd3c3151cd2646173f3ac2750a2a
Muhammed Sinan D <sinanshamsudheen7@gmail.com>
2024-06-30 20:58:00 +0530

    file operations

---
+++ file.py
@@ -0,0 +1,25 @@
+# text files and binary files, both are stored in bits
+
+f = open("demo.txt","w+")
+f.write("this is a new line\n")
+data=f.read()
+print(data)
+f.close()
+
+f=open("demo.txt","a+")
+f.write("this is the next line")
+data=f.read()
+print(data)
+
+f.seek(0)
+data2 = f.readline()
+print(data2)
+f.close()
+
+with open("demo.txt","r") as f:
+    data=f.read()
+    print(data)
+#No need to close the file while using "with"
+
+import os
+os.remove("demo.txt")
