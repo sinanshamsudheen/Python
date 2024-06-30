541484bb2195030a6cfde0d39319eb19bc69ada8
Muhammed Sinan D <sinanshamsudheen7@gmail.com>
2024-07-01 01:51:09 +0530

    prime number

--- prime.py
+++ prime.py
@@ -1,2 +1,14 @@
 n = int(input("Enter the number : "))
+flag=False
+size=(int)(n/2)
+
+for i in range(3,size+1):
+    if(int(i%2)==0):
+        flag=True
+        break
+    
+if(flag==False):
+    print("Is prime")
+else:
+    print("not prime")
 
